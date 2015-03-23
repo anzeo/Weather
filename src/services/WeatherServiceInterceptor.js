d853fc381af6772d19664741f8ad8ac17e4dfe5c
Thomas Anciaux <zebigbozz@gmail.com>
2015-03-23 16:04:54 +0100

    Fixed issue in interceptor
    Interceptor was to eager to fail requests

--- src/services/WeatherServiceInterceptor.js
+++ src/services/WeatherServiceInterceptor.js
@@ -2,7 +2,10 @@ angular.module("weather").factory("WeatherServiceInterceptor", function($q){
     return {
         response: function (response) {
             if(response){
-                return response.data.cod === 200 ? response.data : $q.reject(response);
+                if(response.data.cod) {
+                    return response.data.cod === 200 ? response.data : $q.reject(response);
+                }
+                return response;
             }
             return $q.when(response);
         }
