9338144a9d7016216a6985ff8a87c9ac748fc6d8
Thomas Anciaux <zebigbozz@gmail.com>
2015-03-23 15:31:18 +0100

    Updated interceptor
    Only return the data object

--- src/services/WeatherServiceInterceptor.js
+++ src/services/WeatherServiceInterceptor.js
@@ -2,7 +2,7 @@ angular.module("weather").factory("WeatherServiceInterceptor", function($q){
     return {
         response: function (response) {
             if(response){
-                return response.data.cod === 200 ? response : $q.reject(response);
+                return response.data.cod === 200 ? response.data : $q.reject(response);
             }
             return $q.when(response);
         }
